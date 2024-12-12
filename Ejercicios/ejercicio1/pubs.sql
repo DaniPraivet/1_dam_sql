4c91d904cbe479170ebfdeae69f0229fcdf7ff86
DaniPraivet <133568745+DaniPraivet@users.noreply.github.com>
2024-12-12 20:57:59 +0100

    Update pubs.sql

--- Ejercicios/ejercicio1/pubs.sql
+++ Ejercicios/ejercicio1/pubs.sql
@@ -1,1 +1,20 @@
+-- Creamos la base de datos Pubs
+CREATE DATABASE IF NOT EXISTS pubs;
 
+-- Usamos la base de datos
+USE pubs;
+
+-- Creamos la tabla LOCALIDAD
+CREATE TABLE IF NOT EXISTS localidad (
+  cod_articulo SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY;
+);
+
+-- Creamos la tabla EMPLEADO
+
+-- Creamos la tabla PUB
+
+-- Creamos la tabla TITULAR
+
+-- Creamos la tabla EXISTENCIAS
+
+-- Creamos la tabla PUB_EMPLEADO
