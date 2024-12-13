f23c06bc5c723d7fd05341473cc9eabfded03e63
DaniPraivet <133568745+DaniPraivet@users.noreply.github.com>
2024-12-13 17:28:51 +0100

    Update pubs.sql

--- Ejercicios/ejercicio1/pubs.sql
+++ Ejercicios/ejercicio1/pubs.sql
@@ -6,12 +6,27 @@ USE pubs;
 
 -- Creamos la tabla LOCALIDAD
 CREATE TABLE IF NOT EXISTS localidad (
-  cod_articulo SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY;
+  cod_localidad SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(30) NOT NULL
 );
 
 -- Creamos la tabla EMPLEADO
+CREATE TABLE IF NOT EXISTS empleado(
+  dni_empleado CHAR(9) PRIMARY KEY,
+  nombre VARCHAR(30) NOT NULL,
+  domicilio VARCHAR(50) NOT NULL
+);
 
 -- Creamos la tabla PUB
+CREATE TABLE IF NOT EXISTS pub(
+  cod_pub SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(50) NOT NULL,
+  licencia_fiscal CHAR(9) NOT NULL,
+  domicilio VARCHAR(50) NOT NULL,
+  fecha_apertura DATE NOT NULL,
+  horario VARCHAR(20) CHECK( horario IN ('mañanas','tardes','noches')),
+  cod_localidad 
+);
 
 -- Creamos la tabla TITULAR
 
